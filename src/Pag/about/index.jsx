562db96db5a83b4ddc7fb18a1eeb51576a37419f
MiguelOttoDev <105008843+MiguelOttoDev@users.noreply.github.com>
2024-11-10 01:21:05 -0300

    adwd

--- src/Pag/about/index.jsx
+++ src/Pag/about/index.jsx
@@ -8,7 +8,7 @@ export default function About() {
             <div className={styles.container}>
                 <div className={styles.texto}>
                     <h1>Miguel <span>Otto</span> </h1>
-                    <h3>Sou Miguel Otto, desenvolvedor de software com experiência em tecnologias de front-end e back-end, incluindo React, FlutterFlow, Supabase, MySQL, Firebase, e frameworks como Django e Bootstrap. Atualmente cursando Engenharia de Software pela FIAP e com uma formação técnica pelo Senai, tenho trabalhado em projetos que variam de sistemas de gestão a aplicativos web e interfaces de usuário. Minhas soluções são pensadas para serem escaláveis e intuitivas, sempre aplicando boas práticas de desenvolvimento e metodologias ágeis. Adoro aprender novas tecnologias e criar soluções que simplifiquem o cotidiano das pessoas</h3>
+                    <h3>Sou Miguel Otto, desenvolvedor de software com experiência em tecnologias de front-end e back-end, incluindo React, MySQL, Firebase, e frameworks e Bootstrap. Atualmente cursando Engenharia de Software pela FIAP e com uma formação técnica pelo Senai, tenho trabalhado em projetos que variam de sistemas de gestão a aplicativos web e interfaces de usuário. Minhas soluções são pensadas para serem escaláveis e intuitivas, sempre aplicando boas práticas de desenvolvimento e metodologias ágeis. Adoro aprender novas tecnologias e criar soluções que simplifiquem o cotidiano das pessoas</h3>
                 </div>
                 <div className={styles.img}>
                     <img src={fotoEu} alt="" />
